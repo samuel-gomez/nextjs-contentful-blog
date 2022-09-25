5398d04171302568053d7ae45c5578f86e811aa1
Samuel Gomez <contact@samuelgomez.fr>
2022-09-26 01:24:51 +0200

    fix(revalidate): delete parser

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -2,28 +2,29 @@
 
 export default async function handler(req, res) {
   // should be secret, custom header coming in from Contentful
-  let inboundRevalToken = req.headers['x-vercel-reval-key']
+  let inboundRevalToken = req.headers['x-vercel-reval-key'];
 
   // Check for secret to confirm this is a valid request
   if (!inboundRevalToken) {
     return res
       .status(401)
-      .json({ message: 'x-vercel-reval-key header not defined' })
+      .json({ message: 'x-vercel-reval-key header not defined' });
   } else if (inboundRevalToken !== process.env.CONTENTFUL_REVALIDATE_SECRET) {
-    return res.status(401).json({ message: 'Invalid token' })
+    return res.status(401).json({ message: 'Invalid token' });
   }
 
   try {
-    let postSlug = JSON.parse(req.body).fields.slug['en-US']
+    let postSlug = req.body.fields.slug['en-US'];
+    console.log(postSlug);
 
     // revalidate the individual post and the home page
-    await res.revalidate(`/posts/${postSlug}`)
-    await res.revalidate('/')
+    res.revalidate(`/posts/${postSlug}`);
+    await res.revalidate('/');
 
-    return res.json({ revalidated: true })
+    return res.json({ revalidated: true });
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
-    return res.status(500).send('Error revalidating')
+    return res.status(500).send('Error revalidatingssss');
   }
 }
